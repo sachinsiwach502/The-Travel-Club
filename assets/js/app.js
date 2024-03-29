45e4300310e69e8c7b4e90a68c5f9e2ba808598b
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-03-29 10:12:15 +0530

    make scroll to top

--- assets/js/app.js
+++ assets/js/app.js
@@ -132,4 +132,22 @@ accord.forEach(item => {
         }
 
     })
+});
+
+let to_arrow = document.querySelector(".to_top");
+let pt = 200;
+
+window.addEventListener("scroll", () => {
+    if (scrollY > pt) {
+        to_arrow.style.display = "block"
+    } else {
+        to_arrow.style.display = "none"
+    }
+});
+
+to_arrow.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
 })
